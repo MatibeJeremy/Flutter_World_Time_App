9345c098b387e767ccddff9b17ece7a4bbdf908f
Matibe Alloys <matibe.alloys@students.jkuat.ac.ke>
2020-09-28 10:41:25 +0300

    Included Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:corona_agencies/pages/home.dart';
+import 'package:corona_agencies/pages/choose_location.dart';
+import 'package:corona_agencies/pages/loading.dart';
 
 void main() => runApp(
-      MaterialApp(home: Text('Hello world')),
+      MaterialApp(
+        initialRoute: '/home',
+        routes: {
+          '/': (context) => Home(),
+          '/location': (context) => ChooseLocation(),
+          '/loading': (context) => Loading(),
+        },
+      ),
     );
